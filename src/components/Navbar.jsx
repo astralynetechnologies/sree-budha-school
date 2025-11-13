f02bd24fbe4c95d56f0874894caf63d189938b8d
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2025-11-13 23:09:58 +0530

    removed academic and non academics option from nav under facilites

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -232,7 +232,7 @@ const Navbar = () => {
                   <ChevronDown className="w-4 h-4 ml-1 transition-transform group-hover:rotate-180" />
                 </button>
                 <div className="absolute top-full left-0 bg-white shadow-xl rounded-lg py-3 w-48 z-50 border border-gray-100 opacity-0 invisible group-hover:opacity-100 group-hover:visible transform translate-y-2 group-hover:translate-y-0 transition-all duration-300 ease-out">
-                  <Link
+                  {/* <Link
                     href="/facilities/academics"
                     className="block px-4 py-2 text-gray-800 hover:text-secondary hover:bg-gray-50 transition-all duration-200"
                     onClick={closeDropdown}
@@ -245,7 +245,7 @@ const Navbar = () => {
                     onClick={closeDropdown}
                   >
                     Non-Academics
-                  </Link>
+                  </Link> */}
                   <Link
                     href="/facilities/smartclass"
                     className="block px-4 py-2 text-gray-800 hover:text-secondary hover:bg-gray-50 transition-all duration-200"
@@ -567,7 +567,7 @@ const Navbar = () => {
                   </button>
                   <div className={`overflow-hidden transition-all duration-300 ease-in-out ${activeMobileDropdown === "Facilities" ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}>
                     <div className="bg-gray-50 border-l-2 border-[#0D47A1] ml-4">
-                      <Link
+                      {/* <Link
                         href="/facilities/academics"
                         className="block px-8 py-2 text-gray-700 hover:bg-gray-100 transition-colors"
                         onClick={closeMobileMenu}
@@ -580,7 +580,7 @@ const Navbar = () => {
                         onClick={closeMobileMenu}
                       >
                         Non-Academics
-                      </Link>
+                      </Link> */}
                       <Link
                         href="/facilities/smartclass"
                         className="block px-8 py-2 text-gray-700 hover:bg-gray-100 transition-colors"
